febccbf3b283d1bffdbbed3384be5debf4e77923
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-11-21 14:03:27 +0100

    ADD: Project Carousel, RM: Lengthy words, ADJ: Contact Section, ADD: Overlay

--- app.js
+++ app.js
@@ -25,18 +25,16 @@ const menuIcon1 = document.querySelector(".menu--icon--1");
 const menuIcon2 = document.querySelector(".menu--icon--2");
 const menuIcon3 = document.querySelector(".menu--icon--3");
 
-// Overlay
-const overlay = document.querySelector(".overlay");
-const main = document.querySelector("main");
+const allSections = document.querySelectorAll("main section");
 
 // get the nav bar to add fixed position
 const nav = document.querySelector("nav");
 
-// Menu Update ftn
-function menuUpdate() {
-  // main.appendChild(overlay);
-  // main.classList.toggle("overlay-added");
+const arrowLeft = document.querySelector(".arrow.left");
+const arrowRight = document.querySelector(".arrow.right");
 
+// MENU Update ftn
+function menuUpdate() {
   menuListSpan.classList.toggle("menu-display");
   document.body.classList.toggle("menu-display");
 
@@ -48,20 +46,92 @@ function menuUpdate() {
   icons.forEach((icon) => {
     icon.style.transition = "ease-in 0.5s";
   });
+
+  document.body.classList.toggle("overlay");
 }
 
-// Update year at the footer
+// remove OVERLAY
+function removeOverlay() {
+  if (document.body.classList.contains("overlay")) {
+    allSections.forEach((section) => {
+      section.style.opacity = "1";
+    });
+
+    menuListSpan.classList.remove("menu-display");
+    menuIconSpan.classList.remove("menu-display");
+
+    menuIcon3.classList.remove("hide-third-icon");
+    menuIcon2.classList.remove("rotate-icon-2");
+    menuIcon1.classList.remove("rotate-icon-1");
+    document.querySelector(".menu--container").style.marginTop = "0";
+  }
+}
+
+// Update year at the FOOTER
 const footerYear = document.querySelector(".year");
 const date = new Date();
 const currentYear = date.getFullYear();
 footerYear.textContent = currentYear;
 
-// Event listeners
+// listen for when the nav bar reaches the top of the screen
+const topOfNav = nav.offsetTop;
 
-// event listener on the menu icon span
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    allSections.forEach((section) => {
+      section.style.paddingTop = `${nav.offsetHeight}px`;
+    });
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.querySelector("main").style.paddingTop = 0;
+    allSections.forEach((section) => {
+      section.style.paddingTop = 0;
+    });
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+// PROJECT SLIDER
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const nextSlide = document.querySelector("#next");
+const previousSlide = document.querySelector("#previous");
+
+let curSlide = 0;
+let maxSlide = slides.length - 1;
+
+function nextSlideFtn() {
+  // once we hit the max slides, start afresh
+  if (curSlide === maxSlide) curSlide = 0;
+  else curSlide++;
+
+  slides.forEach((slide, idx) => {
+    slide.style.transform = `translateX(${100 * (idx - curSlide)}%)`;
+  });
+
+  clearInterval(window.onload(autoSlide));
+}
+
+// Next button
+nextSlide.addEventListener("click", nextSlideFtn);
+
+// Previous button
+previousSlide.addEventListener("click", function () {
+  // if the current slide is the first slide, then the max slide should be ===curSlide(this is to ensure the previous slide keeps going backwards)
+
+  if (curSlide === 0) curSlide = maxSlide;
+  else curSlide--;
+
+  slides.forEach((slide, idx) => {
+    slide.style.transform = `translateX(${100 * (idx - curSlide)}%)`;
+  });
+});
+
+// Event listeners
+// on the menu icon span
 menuIconSpan.addEventListener("click", menuUpdate);
 
-// Make the icons close with the Escape key
+// icons close with Escape key
 document.addEventListener("keydown", function (e) {
   console.log(e.key);
   if (e.key === "Escape") menuUpdate();
@@ -70,27 +140,16 @@ document.addEventListener("keydown", function (e) {
 });
 
 // Menu closes when clicked outside the box
-overlay.addEventListener("click", menuUpdate);
-
-// listen for when the nav bar reaches the top of the screen
-const topOfNav = nav.offsetTop;
-const sections = document.querySelectorAll("section");
-
-function fixNav() {
-  console.log(window.scrollY, topOfNav);
-
-  if (window.scrollY >= topOfNav) {
-    sections.forEach((section) => {
-      section.style.paddingTop = `${nav.offsetHeight}px`;
-    });
-    document.body.classList.add("fixed-nav");
-  } else {
-    document.querySelector("main").style.paddingTop = 0;
-    sections.forEach((section) => {
-      section.style.paddingTop = 0;
-    });
-    document.body.classList.remove("fixed-nav");
-  }
-}
+allSections.forEach((section) =>
+  section.addEventListener("click", removeOverlay)
+);
 
 window.addEventListener("scroll", fixNav);
+window.onload = (autoSlide) => {
+  setInterval(() => {
+    nextSlideFtn();
+  }, 2000);
+  setTimeout(() => {
+    nextSlideFtn();
+  }, 10000);
+};
